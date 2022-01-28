fe74a694602c41d1506880c57a21931f8b1a279e
Rafaelo Antonov <ralf_antonov@abv.bg>
2022-01-28 02:59:46 +0200

    firebase config initialized

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjGPZYfcpvQVVx78RY7lxWGqWljcY7gt0",
+  authDomain: "task-manager-cf749.firebaseapp.com",
+  projectId: "task-manager-cf749",
+  storageBucket: "task-manager-cf749.appspot.com",
+  messagingSenderId: "287510978983",
+  appId: "1:287510978983:web:e35bd1aae54e2653949682",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const firestore = firebase.firestore();
+const auth = firebase.auth();
+
+const timestamp = firebase.firestore.Timestamp;
+
+export { firestore, auth, timestamp };
