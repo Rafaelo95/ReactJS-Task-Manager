586c9a61e1fc897fc3691eb382de1da3dcac3eaf
Rafaelo Antonov <ralf_antonov@abv.bg>
2022-01-28 04:41:13 +0200

    pages + routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// styles
+import "./App.css";
+
+import Dashboard from "./components/dashboard/Dashboard";
+import Create from "./components/create/Create";
+import Login from "./components/login/Login";
+import Project from "./components/project/Project";
+import Signup from "./components/signup/Signup";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/project/:id" element={<Project />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
