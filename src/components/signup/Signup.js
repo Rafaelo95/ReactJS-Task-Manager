df3698e84c50ec1b5b8ecc0776d0b52113e2672f
Rafaelo Antonov <ralf_antonov@abv.bg>
2022-11-29 05:50:37 +0200

    Update Signup.js

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -1,18 +1,42 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 // styles
-import "./Signup.css";
+import './Signup.css'
 
 export default function Signup() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [displayName, setDisplayName] = useState("");
-  const [thumbnail, setThumbnail] = useState(null);
-
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
+  const [thumbnail, setThumbnail] = useState(null)
+  const [thumbnailError, setThumbnailError] = useState(null)
+  
   const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(email, password, displayName);
-  };
+    e.preventDefault()
+    console.log(email, password, displayName, thumbnail)
+  }
+
+  const handleFileChange = (e) => {
+    setThumbnail(null)
+    let selected = e.target.files[0]
+    console.log(selected)
+
+    if (!selected) {
+      setThumbnailError('Please select a file')
+      return
+    }
+    if (!selected.type.includes('image')) {
+      setThumbnailError('Selected file must be an image')
+      return
+    }
+    if (selected.size > 100000) {
+      setThumbnailError('Image file size must be less than 100kb')
+      return
+    }
+    
+    setThumbnailError(null)
+    setThumbnail(selected)
+    console.log('thumbnail updated')
+  }
 
   return (
     <form onSubmit={handleSubmit} className="auth-form">
@@ -20,9 +44,9 @@ export default function Signup() {
       <label>
         <span>email:</span>
         <input
-          required
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
+          required 
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)} 
           value={email}
         />
       </label>
@@ -30,8 +54,8 @@ export default function Signup() {
         <span>password:</span>
         <input
           required
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
           value={password}
         />
       </label>
@@ -39,16 +63,21 @@ export default function Signup() {
         <span>display name:</span>
         <input
           required
-          type="text"
-          onChange={(e) => setDisplayName(e.target.value)}
+          type="text" 
+          onChange={(e) => setDisplayName(e.target.value)} 
           value={displayName}
         />
       </label>
       <label>
         <span>Profile thumbnail:</span>
-        <input required type="file" />
+        <input 
+          required
+          type="file"
+          onChange={handleFileChange}
+        />
+        {thumbnailError && <div className="error">{thumbnailError}</div>}
       </label>
       <button className="btn">Sign up</button>
     </form>
-  );
+  )
 }
